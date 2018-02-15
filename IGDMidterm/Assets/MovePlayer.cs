ca809d3b59192a3d7385866c21bd9848a7d70fd2
Tyler <tjt288@nyu.edu>
2018-02-15 10:50:48 -0500

    Basic Player Movement

---
+++ IGDMidterm/Assets/MovePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+	private Rigidbody playerBody;
+	public float forwardForce;
+	public float backwardForce;
+	
+	// Use this for initialization
+	void Start () {
+		playerBody = this.gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+		{
+			playerBody.AddForceAtPosition(new Vector3(0,0,forwardForce), transform.position + new Vector3(0,.1f,.1f));
+		}
+		
+		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.A))
+		{
+			playerBody.AddForceAtPosition(new Vector3(0,0,backwardForce), transform.position + new Vector3(0,.1f,-.1f));
+		}
+		
+	}
+}
