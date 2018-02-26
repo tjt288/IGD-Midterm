d9f9a9ccebe720cc45314f7a22accf25fbc2e95b
Tyler <tjt288@nyu.edu>
2018-02-26 16:33:27 -0500

    Fixed shooting and added start screen

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -19,7 +19,6 @@ public class Shoot : MonoBehaviour {
 			GameObject.Find("Puck").transform.position = this.transform.position + this.transform.forward/3;
 			if (Input.GetKey(KeyCode.Mouse0))
 			{
-				GameObject.Find("Player").GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
 				GameObject.Find("Puck").GetComponent<Rigidbody>().AddForce((GameObject.Find("Puck").transform.position - this.transform.position) * 50, ForceMode.Impulse);
 				carryPuck = false;
 			}
